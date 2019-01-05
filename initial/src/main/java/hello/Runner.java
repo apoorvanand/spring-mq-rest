a37b2a8278d8921fd4464ee7e7bece3b994f4720
apoorvanand <apoorwanand@gmail.com>
2019-01-06 04:26:51 +0530

    integrated rest api with mq

---
+++ initial/src/main/java/hello/Runner.java
@@ -0,0 +1,50 @@
+package hello;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class Runner implements CommandLineRunner {
+    public static double getRandomIntegerBetweenRange(double min, double max){
+        double x = (int)(Math.random()*((max-min)+1))+min;
+        return x;
+    }
+    private final RabbitTemplate rabbitTemplate;
+    private final Receiver receiver;
+    private String server = "https://locale-apoorvanand.b542.starter-us-east-2a.openshiftapps.com/search/";
+  private RestTemplate rest;
+  private HttpHeaders headers;
+
+    public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
+        this.receiver = receiver;
+        this.rabbitTemplate = rabbitTemplate;
+        this.rest = new RestTemplate();
+    this.headers = new HttpHeaders();
+    headers.add("Content-Type", "application/json");
+    headers.add("Accept", "*/*");
+    }
+    public String get() {
+        HttpEntity<String> requestEntity = new HttpEntity<String>("", headers);
+        ResponseEntity<String> responseEntity = rest.exchange(server+getRandomIntegerBetweenRange(1,100), HttpMethod.GET, requestEntity, String.class);
+       // this.setStatus(responseEntity.getStatusCode());
+        return responseEntity.getBody();
+      }
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+        System.out.println("Sending message...");
+        rabbitTemplate.convertAndSend(Application.topicExchangeName, "foo.bar.baz", "Hello from Zypher"+get());
+        receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+    }
+
+}
